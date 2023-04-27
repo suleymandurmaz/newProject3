b9539897fe505141549dbdd09c18d5300ea9442f
suleymandurmaz <sdurmaz77@hotmail.com>
2023-04-27 21:49:16 +0300

    cucumber report plugin eklendi

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -6,7 +6,12 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         features = "src/test/java/features/Deneme.feature",
-        glue="stefdefs"
+        glue="stefdefs",
+        plugin = {"pretty",
+                "json:test-output/cucumber-reports/cucumber.json",
+                "html:test-output/cucumber-reports/cucumberreport.html",
+        }
+
 )
 public class Runner extends AbstractTestNGCucumberTests {
 }
